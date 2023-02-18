61fd2e906547be6763fb56c8e57b9d2290afc6d9
Bryce Watson <brycewatson315@gmail.com>
2023-02-18 12:26:29 -0500

    hero post fix

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,38 +1,27 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
 import CoverImage from './cover-image'
 import Link from 'next/link'
-import type Author from '../interfaces/author'
 import { Typography } from '@mui/material'
 
 type Props = {
   title: string
   coverImage: string
   excerpt: string
-  slug: string
 }
 
 const HeroPost = ({
   title,
   coverImage,
   excerpt,
-  slug,
 }: Props) => {
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
+        <CoverImage title={title} src={coverImage} />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <Typography variant='h4'>
-          <Link
-              as={`/work/${slug}`}
-              href="/work/[slug]"
-              className="hover:underline"
-            >
-              {title}
-            </Link>
+            {title}
           </Typography>
         </div>
         <div>
