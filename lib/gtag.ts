cb5bad0abaa7bc8433c533e50a7db7012b043290
Bryce Watson <brycewatson315@gmail.com>
2023-02-20 17:29:20 -0500

    checking for window.gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,9 +2,11 @@ export const GA_TRACKING_ID = process.env.GTAG_CODE;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url
-  });
+  if (typeof window !== 'undefined') {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_location: url,
+    });
+  }
 };
 
 type GTagEvent = {
@@ -16,9 +18,11 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value
-  });
+  if (typeof window !== 'undefined') { 
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value
+    });
+  }
 };
