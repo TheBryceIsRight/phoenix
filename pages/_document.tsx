8ee0fcdf61ac280dedcaeab175640fca700e2d11
Bryce Watson <brycewatson315@gmail.com>
2023-02-20 17:05:18 -0500

    GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,22 +7,6 @@ export default function Document() {
       <Head >
 
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-95MDFTMREQ" />
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag()
-              {dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', G-95MDFTMREQ, {
-                page_path: window.location.pathname,
-             });
-            `,
-        }}
-      />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
