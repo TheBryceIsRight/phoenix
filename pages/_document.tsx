b243e4f9897fe7a08944dfc88dea7d12c2ca16b3
Bryce Watson <brycewatson315@gmail.com>
2023-02-24 13:27:54 -0500

    silenced console logs in production

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head >
-
       </Head>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
       />
       <body>
-
         <Main />
         <NextScript />
       </body>
