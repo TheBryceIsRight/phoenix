beb8d7a460201d9a5c3b151d76d11d493830179b
Bryce Watson <brycewatson315@gmail.com>
2023-02-24 13:30:15 -0500

    fixed next.js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // @type {import('next').NextConfig}
 
-export const nextConfig = {
-    compiler: {
-      removeConsole: process.env.NODE_ENV === "production"
-    },
-  }
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production"
+  },
+}
+
+export default nextConfig
