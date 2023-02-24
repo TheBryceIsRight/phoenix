258585a9f85ed2ed0b64e63553fab3bd688c24c3
Bryce Watson <brycewatson315@gmail.com>
2023-02-24 13:09:21 -0500

    removed Roboto script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-
-      </Head>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
       <body>
-
         <Main />
         <NextScript />
       </body>
