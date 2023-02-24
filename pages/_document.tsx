f9c729611a8dda0b3833a6ff6b17c71863979882
Bryce Watson <brycewatson315@gmail.com>
2023-02-24 13:11:56 -0500

    reverting previous commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,18 @@
-import { Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head >
+
+      </Head>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
       <body>
+
         <Main />
         <NextScript />
       </body>
