ddb0ac3da8a6e534be14a7f3a55d2280c5b20e4d
Bryce Watson <bryce@gitkraken.com>
2023-12-30 15:50:01 -0500

    Updated meta images

--- components/meta.tsx
+++ components/meta.tsx
@@ -42,14 +42,14 @@ const Meta = () => {
       <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
       <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
       <meta property="twitter:image:alt" content="laptop with user interface and the AllVoices logo"></meta>
-      <meta property="twitter:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1672785472/AllVoices_pyvu6b.png"></meta>
-      <meta property="og:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1672785472/AllVoices_pyvu6b.png"></meta>
+      <meta property="twitter:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1703969210/GitKraken.png"></meta>
+      <meta property="og:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1703969210/GitKraken.png"></meta>
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta property="twitter:title" content="Bryce Watson's Design Portfolio"></meta>
-      <meta property="twitter:description" content="A history of my work as a User Experience Designer from Georgia Tech until now"></meta>
+      <meta property="twitter:description" content="A summary of my work as a Product Designer"></meta>
       <meta property="og:image:type" content="image/png"></meta>
       <meta property="og:title" content="Bryce Watson's Design Portfolio"></meta>
-      <meta property="og:description" content="A history of my work as a User Experience Designer from Georgia Tech until now"></meta>
+      <meta property="og:description" content="A summary of my work as a Product Designer"></meta>
       <meta property="og:image:width" content="1446"></meta>
       <meta property="og:site_name" content="Bryce Watson's Design Portfolio"></meta>
       <meta property="og:image:height" content="1024"></meta>
